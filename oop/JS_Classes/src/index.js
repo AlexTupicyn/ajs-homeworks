96dfa4534bd5b510d67f1f4ed8d16d0556a48e6d
Александр <95536279+AlexTupicyn@users.noreply.github.com>
2025-05-20 10:51:48 +0300

    Delete oop/JS_Classes directory

--- oop/JS_Classes/src/index.js
+++
@@ -1,5 +0,0 @@
-import './css/style.css';
-
-import './js/character.js';
-
-// TODO: write your code in app.js
--- oop/JS_Classes/src/js/swordsman.js
+++
@@ -1,7 +0,0 @@
-import Character from './character.js';
-
-export default class Swordsman extends Character {
-    constructor(name) {
-        super(name, 'Swordsman', 40, 10);
-    }
-}
